3568319dead36d9214b10ae7333550706e0d7da1
krmboya <krmboya@gmail.com>
2012-12-07 18:58:33 +0300

    Minor corrections

--- dbg.h
+++ dbg.h
@@ -1,5 +1,5 @@
 #ifndef __dbg_h__
-#def __dbg_h__
+#define __dbg_h__
 
 #include <stdio.h>
 #include <errno.h>
@@ -14,7 +14,7 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 #define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s)" M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 #define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno %s)" M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d: errno %s)" M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:)" M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 #define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
